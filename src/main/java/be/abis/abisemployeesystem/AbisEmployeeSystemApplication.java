c089741b5e8d0efb8aff03f3f08f48d30d4c1210
Jana Heitkemper <janaheitkemper@gmail.com>
2022-12-22 14:40:08 +0100

    first models, first commit

---
+++ src/main/java/be/abis/abisemployeesystem/AbisEmployeeSystemApplication.java
@@ -0,0 +1,13 @@
+package be.abis.abisemployeesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbisEmployeeSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AbisEmployeeSystemApplication.class, args);
+    }
+
+}
